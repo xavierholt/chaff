133e737d40346086cea73ca76253d9f77d82db46
Kevin Burk <xavierholt@gmail.com>
2017-11-04 23:35:50 -0700

    Oldschool C++

--- test/example.cpp
+++ test/example.cpp
@@ -4,20 +4,27 @@
 #include <iostream>
 #include <vector>
 
+typedef Chaff::MaxFinder<int,int> MyFinder;
+const int N = 1000000;
+
 int main() {
-  std::vector<int> things(1000000);
-  for(int& i: things) {
-    i = rand();
+  // ONE MILLION random integers!
+  std::vector<int> things(N);
+  for(int i = 0; i < N; ++i) {
+    things[i] = rand();
   }
 
-  auto finder = Chaff::MaxFinder<int,int>::byCount(3);
-  for(int i: things) {
-    finder.sow(i, i);
+  // Inspect them all to find the best three...
+  MyFinder finder = MyFinder::byCount(3);
+  for(int i = 0; i < N; ++i) {
+    finder.sow(things[i], things[i]);
   }
 
+  // And show us what you found!
+  std::vector<int> found(finder.reap());
   std::cout << "The best three things are:\n";
-  for(int i: finder.reap()) {
-    std::cout << " - " << i << '\n';
+  for(int i = 0; i < 3; ++i) {
+    std::cout << " - " << found[i] << '\n';
   }
 
   std::cout << "Aren't they amazing?\n\n";
